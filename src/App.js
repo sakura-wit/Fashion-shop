df16a3dfcc7187ff8a860c4f93774a264b6f73f6
Danniek2 <dannie022023@gmail.com>
2023-11-01 15:48:38 +0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import './styles/common.css'
 import './styles/checkout.css'
 import './styles/detail.css'
 import './styles/cart.css'
-import './styles/login.css'
-import './styles/forgot.css'
-import './styles/register.css'
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -22,9 +20,7 @@ import { CheckOut } from './Pages/CheckOut';
 import TestForm from './Pages/TestForm';
 import { DetailProductPage } from './Pages/DetailProductPage';
 import { ShoppingCart } from './Pages/ShoppingCart';
-import {LoginPage} from './Pages/Login';
-import { ForgotPage } from './Pages/Forgot';
-import { RegisterPage } from './Pages/Register';
+
 
 
 function App() {
@@ -37,9 +33,7 @@ function App() {
         <Route path='test' element={<TestForm />} />
         <Route path='/detail' element={<DetailProductPage />} />
         <Route path='/cart' element={<ShoppingCart />} />
-        <Route path='/login' element={<LoginPage/>} />
-        <Route path='/forgot' element={<ForgotPage/>} />
-        <Route path='/register' element={<RegisterPage/>} />
+        
 
         {/* <Route path='/detail' element={<DetailPage />} />
         <Route path='/homepage' element={<HomePage />} /> */}
