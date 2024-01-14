bc5fbe44cef3983855b9f0b481712688981f2b00
sakura-wit <datnguyenwit@gmail.com>
2024-01-14 11:32:54 +0700

    deploy1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { render } from "react-dom";
 import './index.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -14,28 +16,27 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 const queryClient = new QueryClient()
 
-root.render(
-  // <React.StrictMode>
-  <Router>
-    <Provider store={store}>
-      {/* <MainLayout> */}
-      <QueryClientProvider client={queryClient}>
 
-        <App />
+const root = document.getElementById("root");
+render(<Router>
+  <Provider store={store}>
+    {/* <MainLayout> */}
+    <QueryClientProvider client={queryClient}>
 
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-      {/* </MainLayout> */}
-    </Provider>
-  </Router>
-  // </React.StrictMode >
-);
+      <App />
+
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+    {/* </MainLayout> */}
+  </Provider>
+</Router>, root);
 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
